99f2a6d741b92488f89bfc4c1b6591d964b360bd
quangquanh <60811649+quangquanh@users.noreply.github.com>
2020-02-09 00:55:41 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -14,7 +14,7 @@ function firstQuestion(){
     
     $('.content').hide();
     Swal.fire({
-        title: 'haloo cậu!',
+        title: 'hallooo cậu!',
         text: 'Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.',
         imageUrl: 'img/cuteCat.jpg',
         imageWidth: 300,
