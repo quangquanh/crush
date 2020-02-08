a47ca30add21b2a5ae817b8b9cfbc20751b8850a
quangquanh <60811649+quangquanh@users.noreply.github.com>
2020-02-09 00:51:42 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -73,7 +73,7 @@ $('#no').click(() => {
 // generate text in input
 function textGenerate() {
     var n = "";
-    var text = " Tại vì cậu da'ng ye^u vl :<<<<<<< ";
+    var text = " Tại vì cậu đáng yêu vl :<<<<<<< ";
     var a = Array.from(text);
     var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
     var count = textVal.length;
